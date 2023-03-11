b7bfe51fad834550489797bcbd70ea2fb5b201e8
Nelson Chukwu <34746074+NelsonTallest@users.noreply.github.com>
2023-03-11 10:27:00 +0100

    Add files via upload

---
+++ OTP.js
@@ -0,0 +1,10 @@
+function generateOTP(){
+  var digit ='0123456789';
+  let OTP='';
+  for( let i =0; i< 6; i++) {
+    OTP += digit[Math.floor(Math.random() + 10)]
+  }
+  return OTP;
+}
+generateOTP()
+generateOTP()
